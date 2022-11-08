7f1635b5569a53036bea198a030836017876f026
antoinePutmans <antoine.putmans@student.vinci.be>
2022-11-08 15:16:01 +0100

    last

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,28 @@
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
+import { Vector3 } from "@babylonjs/core/Maths/math";
+
+import '@babylonjs/core/Materials/standardMaterial';
+
 const HomePage = () => {
-  const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+  const canvas = document.querySelector("canvas");
+  const engine = new Engine(canvas, true);
+
+  const scene = new Scene(engine);
+
+  const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0, 0, 0), scene);
+  camera.attachControl(canvas, true);
+
+  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+
+  const box = MeshBuilder.CreateBox("box", {}, scene);
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
 };
 
 export default HomePage;
--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,7 @@
-import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
-import NewPage from '../Pages/NewPage';
 
 const routes = {
   '/': HomePage,
-  '/game': GamePage,
-  '/new': NewPage,
 };
 
 export default routes;
