b3ce4ef14f13b1943e1cafd5012cb7d3596e7dc6
Kybol <kimberley.511@gmail.com>
2022-11-24 11:48:33 +0100

    adding login and register function but NOT securized

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const usersRouter = require('./routes/users');
-const pizzaRouter = require('./routes/pizzas');
+const authsRouter = require('./routes/auths');
 
 const app = express();
 
@@ -13,6 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/users', usersRouter);
-app.use('/pizzas', pizzaRouter);
+app.use('/auths', authsRouter);
 
 module.exports = app;
