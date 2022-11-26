314fe47c994b66752a6f926be0af3201412cb144
antoinePutmans <antoine.putmans@student.vinci.be>
2022-11-26 12:01:29 +0100

    refactor: leaderboard page

--- frontend/src/Components/Pages/Leaderboard.js
+++ frontend/src/Components/Pages/Leaderboard.js
@@ -1,66 +1,50 @@
 import { clearPage } from '../../utils/render';
 
 const Leaderboard = async () => {
-    try {
-        clearPage();
+  try {
+    clearPage();
 
-        const response = await fetch('/api/users');
+    const response = await fetch('/api/users');
 
-        if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+    if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
 
-        const users = await response.json();
-        renderUsersFromString(users);
-
-    } catch (err) {
-
+    const users = await response.json();
+    renderUsersFromString(users);
+  } catch (err) {
     console.error('Leaderboard error ', err);
-    }
+  }
+};
+
+function renderUsersFromString(users) {
+  const usersTableAsString = getAllTableLinesAsString(users);
+
+  const main = document.querySelector('main');
+
+  main.innerHTML += usersTableAsString;
 }
 
 
-    function renderUsersFromString(users) {
-        const usersTableAsString = getUsersTableAsString(users);
-    
-        const main = document.querySelector('main');
-    
-        main.innerHTML += usersTableAsString;
-    }
+function getAllTableLinesAsString(listUsers) {
+  let counter = 0;
 
-
-    function getUsersTableAsString(user) {
-        const userTableLines = getAllTableLinesAsString(user);
-        const users = addLinesToTableHeadersAndGet(userTableLines);
-        return users;
-      }
-
-    function addLinesToTableHeadersAndGet(tableLines) {
-        const userTable = `
-        <div class="p-5 text-center bg-light">
+  let usersLines = `
         <h1 class="mb-3">Leaderboard</h1>
-   
-        ${tableLines}`;
-        return userTable;
-    }
-
-    function getAllTableLinesAsString(listUsers) {
-        let counter = 0;
-        let usersLines = `<table>
+        <table class "table-auto">
         <tbody>`;
-      
-     
-        listUsers?.forEach((user) => {
-            counter++;
-            usersLines += `<tr>
+
+  listUsers?.forEach((user) => {
+    counter++;
+    usersLines += `<tr>
                 <td class="fw-bold text-info" >${counter}</td>
                 <td class="fw-bold text-info" >${user.username}</td>
                 <td class="fw-bold text-info" >${user.highscore}</td>
             </tr>`;
-        });
-        
-     usersLines +=`</tbody>
-    </table>`
+  });
 
-    return usersLines;
-    }
+  usersLines += `</tbody>
+    </table>`;
 
-  export default Leaderboard;
+  return usersLines;
+}
+
+export default Leaderboard;
