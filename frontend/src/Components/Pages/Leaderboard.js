193c499dfceb04867628ad5f084d6b02660cb312
antoinePutmans <antoine.putmans@student.vinci.be>
2022-11-27 10:47:35 +0100

    leaderboard css in progress

--- frontend/src/Components/Pages/Leaderboard.js
+++ frontend/src/Components/Pages/Leaderboard.js
@@ -27,28 +27,46 @@ function getAllTableLinesAsString(listUsers) {
   let counter = 0;
 
   let usersLines = `
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Leaderboard</h1>
-        <table class="table-auto">
-        <thead>
-        <tr>
-        <th>Rank</th>
-        <th>Name</th>
-        <th>Score</th>
-        </tr>
-        </thead>
-        <tbody>`;
+  <div class="py-10 inline-block min-w-full sm:px-6 lg:px-8">
+  <h1 class="text-center mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Leaderboard</h1>
+  </di>
+
+ 
+     
+        <table class="text-center min-w-full">
+          <thead class="border-b">
+            <tr class="bg-emerald-300">
+              <th  class="text-sm font-bold text-gray-900 px-6 py-4">
+              Rank
+              </th>
+              <th  class="text-sm font-bold text-gray-900 px-6 py-4">
+              Name
+              </th>
+              <th  class="text-sm font-bold text-gray-900 px-6 py-4">
+              Score
+              </th>
+            </tr>
+          </thead>
+        <tbody>
+       `;
 
   listUsers?.forEach((user) => {
     counter++;
-    usersLines += `<tr>
-                <td>${counter}</td>
-                <td>${user.username}</td>
-                <td>${user.highscore}</td>
-            </tr>`;
+    usersLines += `
+    <tr class="border-b bg-emerald-300">
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${counter}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${user.username}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${user.highscore}</td>
+    </tr>
+    `;
   });
 
-  usersLines += `</tbody>
-    </table>`;
+  usersLines += `
+   </tbody>
+  </table>
+</div>
+</div>
+`;
 
   return usersLines;
 }
