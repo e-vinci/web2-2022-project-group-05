3a3c8d2e7c333c9425894328b12a3a3b4b31e683
Kybol <kimberley.511@gmail.com>
2022-12-11 19:18:29 +0100

    opti textures

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -3,6 +3,9 @@ import { clearPage, renderPageTitle } from '../../utils/render';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
 import Navigate from '../Router/Navigate';
+import rope01 from '../../assets/img/rope_01.png';
+import rope02 from '../../assets/img/rope_02.png';
+import rope03 from '../../assets/img/rope_03.png';
 
 const LoginPage = () => {
   clearPage();
@@ -23,14 +26,22 @@ const LoginPage = () => {
   function renderLoginForm(){
     const form = `
     <div class="pb-10 inline-block min-w-full sm:px-6 lg:px-60">
-      <form class="bg-custom-blue px-10 pt-6 pb-8 mb-4 rounded-3xl">
+      <form class="relative bg-custom-blue px-10 pt-6 pb-8 mb-4 rounded-3xl">
+      <div class="absolute left-0 px-0 py-0 flex flex-row">
+        <div class="h-20 w-28">
+          <img src="${rope01}" class="object-scale-down">
+        </div>
+        <div class="w-20 h-32">
+          <img src="${rope02}" class="object-scale-down">
+        </div>
+        </div>
         <div class="flex flex-col justify-center">
             <label class="bg-wood-board-01 bg-cover bg-center block text-white text-center text-xl font-mono mt-10 w-1/5 h-15" for="username">username :</label>
             <input  id="username" class="bg-custom-lightyellow shadow appearance-none rounded w-1/2 py-2 px-3" name="username" type="text">
             <label class="bg-wood-board-01 bg-cover bg-center block text-white text-center text-xl font-mono mt-10 w-1/5 h-15" for="password">password :</label>
             <input  id="password" class="bg-custom-lightyellow shadow appearance-none rounded w-1/2 py-2 px-3" name="password" type="password">
         </div>
-        <div class="flex flex-rox justify-between static overflow-visible mt-100">
+        <div class="flex flex-row justify-between static overflow-visible mt-100">
           <button id="register-redirection" class="bg-wood-board-02 hover:text-custom-blue bg-cover bg-left mt-10 text-white text-xl font-mono py-5 px-10">register</button>
           <input class="bg-wood-board-02 hover:text-custom-blue bg-cover bg-left mt-10 text-white text-xl font-mono py-5 px-10" type="submit" value="login">
         </div>
@@ -40,7 +51,6 @@ const LoginPage = () => {
 
     return form;
   }
-
   async function onLogin(e) {
   e.preventDefault();
 
