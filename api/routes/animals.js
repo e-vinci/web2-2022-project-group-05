fe5920126f92b0f3f8302477c645f406c01a3923
antoinePutmans <antoine.putmans@student.vinci.be>
2022-12-18 12:02:11 +0100

    fixing animal request :)

--- api/routes/animals.js
+++ api/routes/animals.js
@@ -1,10 +1,10 @@
 const express = require('express');
-// const animals = require('../models/animals');
+const animals = require('../models/animals');
 
 const router = express.Router();
 
 
-// router.get('/', (res) => res.json({ animal: animals.getRandomAnimal()})
-// )
+router.get('/', (req,res) => res.json({ animal: animals.getRandomAnimal()})
+)
 
 module.exports = router;
